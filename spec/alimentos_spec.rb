ac246348c970881b2fc21ab4e609d781909a7309
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-08 09:34:56 +0000

    Se crea una instancia de la clase alimentos y se les pasan los parámetros necesarios

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -1,5 +1,10 @@
 RSpec.describe Alimentos do
-  it "Has a version number" do
-    expect(Alimentos::VERSION).not_to be nil
-  end
+
+	before :all do
+    		@alimento = Comida.new("Kellogs",100.0,30.0,7.0,4.0,2.0,1.0,71.0,36.0,0.0,46.0,4.0,0.0,0.9,1.0)
+  	end  
+
+  	it "Has a version number" do
+    		expect(Alimentos::VERSION).not_to be nil
+ 	 end
 end
