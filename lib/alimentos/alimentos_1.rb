611611bbe05c00b361b0085bc853445f367612c9
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-08 09:37:03 +0000

    c3:Se crea el initialize con los valores necesarios para la realización de la práctica

--- lib/alimentos/alimentos_1.rb
+++ lib/alimentos/alimentos_1.rb
@@ -1,4 +1,21 @@
 require "alimentos/version"
 
 class Comida
+	def initialize(nombre,cantidad,porcion,grasas,grasas_saturadas,grasas_monosaturadas,grasas_polisaturadas,hidratos_carbono,azucares,polialcoholes,almidon,fibra,proteinas,sal,vitaminas_mineral)
+        	@nombre=nombre
+        	@cantidad=cantidad
+        	@porcion=porcion
+        	@grasas = grasas
+        	@grasas_saturadas=grasas_saturadas
+        	@grasas_monosaturadas = grasas_monosaturadas
+        	@grasas_polisaturadas=grasas_polisaturadas
+        	@hidratos_carbono=hidratos_carbono
+        	@azucares=azucares
+        	@polialcoholes=polialcoholes
+        	@almidon=almidon
+        	@fibra=fibra
+        	@proteinas=proteinas
+        	@sal=sal
+        	@vitaminas_mineral=vitaminas_mineral
+    	end
 end
