65995473f4ac0dfe51913ee9e0532952c31238c1
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-08 10:31:09 +0000

    Se crea una prueba que comprube que existe un método que devuelve la cantidad de azúcares y esta es correcta

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -79,8 +79,11 @@ RSpec.describe Alimentos do
 		it "Existe un método para obtener la cantidad de grasas polisaturadas" do
       			expect(@alimento.grasas_polisaturadas).to eq(1.0)
     		end
-		 it "Existe un método para obtener la cantidad de hidratos de carbono" do
+		it "Existe un método para obtener la cantidad de hidratos de carbono" do
       			expect(@alimento.hidratos_carbono).to eq(71.0)
+    		end
+		it "Existe un método para obtener la cantidad de azucares" do
+      			expect(@alimento.azucares).to eq(36.0)
     		end
 	end
 
