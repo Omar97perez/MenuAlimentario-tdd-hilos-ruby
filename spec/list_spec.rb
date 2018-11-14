b8f46cc1cebdfeb94032c76964494c97665a91e0
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-14 19:43:42 +0000

    Comprobación nodo siguiente y anterior clase nodo

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -12,8 +12,25 @@ RSpec.describe Node do
 		it "Node 2 tiene valor 2" do
             		expect(@node2.value).to eq(2)
         	end
-    		end
+    	end
 
+	describe "Prueba anterior y siguiente a los nodos:" do
+        	it "Debe existir un nodo siguiente a nodo 1 " do
+            		expect(@node1.next).not_to be nil
+        	end
+
+		it "Debe existir un nodo anterior a nodo 2 " do
+            		expect(@node2.prev).not_to be nil
+        	end
+
+		it "El nodo que le sigue a nodo 1 es el nodo 2" do
+            		expect(@node1.next.value).to eq(2)
+        	end
+
+		it "El nodo que anterior a nodo 2 es el nodo 1" do
+            		expect(@node2.prev.value).to eq(1)
+        	end
+	end
 end
 
 RSpec.describe Lista do
