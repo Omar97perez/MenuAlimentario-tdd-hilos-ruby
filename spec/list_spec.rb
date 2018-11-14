bc7ac0550cf91e7bf07050be2e3cbf42ee8009b9
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-14 19:40:58 +0000

    Prueba valor de dos nodos

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe Node do
         	@node2=Node.new(2, nil, @node1)
         	@node1.next=@node2
     	end
+
+	describe "Prueba valor nodos:" do
+        	it "Node 1 tiene valor 1" do
+            		expect(@node1.value).to eq(1)
+        	end 
+		it "Node 2 tiene valor 2" do
+            		expect(@node2.value).to eq(2)
+        	end
+    		end
+
 end
 
 RSpec.describe Lista do
