199c49ee70b0fb10604995b9ef411b29eff00904
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-21 17:57:00 +0000

    Creando estructura de la Práctica 8

--- lib/alimentos.rb
+++ lib/alimentos.rb
@@ -1,6 +1,7 @@
 require "alimentos/version" 
 require_relative "../lib/alimentos/alimentos_1.rb" 
 require_relative "../lib/alimentos/alimentos_2.rb"
+require_relative "../lib/alimentos/alimentos_3.rb"
 
 module Alimentos
 
