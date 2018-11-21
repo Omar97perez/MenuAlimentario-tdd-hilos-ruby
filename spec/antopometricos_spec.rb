a62dfa31b96f2757c5fba24d0d67c8bca04960af
Omar Pérez Znakar <alu0100945645@ull.edu.es>
2018-11-21 20:07:06 +0000

    Creación pruebas para cuenado el imc es menor que 24.9

--- spec/antopometricos_spec.rb
+++ spec/antopometricos_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe Operaciones do
     
     	before :all do
-        	@sujeto1 = Operaciones.new("Omar","79062976Q",65,1.72,"12:00",20,1,[82.0,82.9],[82.0,82.9],[10.5,10.0,11.0],[25.5,26.0,25.9],[17.0,17.0,17.9],[17.0,17.0,17.9],[10.0,11.0])
-             	@lista= Lista.new(nil,nil)
-        	@lista.insertEnd(@sujeto1)
+		@sujeto1 = Operaciones.new("Omar","79062976Q",65,1.72,"12:00",20,1,[82.0,82.9],[82.0,82.9],[10.5,10.0,11.0],[25.5,26.0,25.9],[17.0,17.0,17.9],[17.0,17.0,17.9],[10.0,11.0])
+        	@sujeto2 = Operaciones.new("Omar","79062976Q",47,1.62,"12:00",19,0,[70.0,70.9],[82.0,82.9],[10.5,10.0,11.0],[25.5,26.0,25.9],[17.0,17.0,17.9],[25.5,26.0,25.9],[10.0,11.0])
+        	@sujeto3 = Operaciones.new("Omar","79062976Q",49,1.67,"12:00",16,0,[72.0,72.9],[82.0,82.9],[10.5,10.0,11.0],[25.5,26.0,25.9],[17.0,17.0,17.9],[25.5,26.0,25.9],[10.0,11.0])
+        	@sujeto4 = Operaciones.new("Omar","79062976Q",65,1.72,"12:00",20,1,[82.0,82.9],[82.0,82.9],[10.5,10.0,11.0],[25.5,26.0,25.9],[17.0,17.0,17.9],[17.0,17.0,17.9],[10.0,11.0])
+        	@sujeto5 = Operaciones.new("Omar","79062976Q",63,1.70,"12:00",18,1,[81.0,81.9],[83.0,83.9],[12.5,12.0,13.0],[23.5,22.0,23.9],[18.0,18.0,17.9],[17.0,17.0,17.9],[10.0,11.0])
+        	@lista= Lista.new(nil,nil)
+        	@lista.insertEnd(@sujeto1,@sujeto2,@sujeto3,@sujeto4,@sujeto5)
 	end
 
 	describe "Pruebas de clase Datos" do
@@ -72,6 +76,17 @@ RSpec.describe Operaciones do
         	end
     		it "El objeto puede usar la función pop_last" do
             		expect(@lista.respond_to?('pop_last')).to eq(true)
+        	end
+    	end
+
+	describe "Clasificación según imc:" do 
+        	c=0
+        	while c < 4
+            		it "Elemento con menos de 29.9 de imc" do
+                		expect(@lista.last.value.imc).to be < (24.9)
+                		@lista.pop_last
+            		end
+            		c+=1
         	end
     	end
 end
