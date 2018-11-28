6a586f948ac2daca2218061d94cf34fa0abb2b55
Omar <omarperezznakar@gmail.com>
2018-11-28 00:12:15 +0000

    Before all Información Nutricional

--- spec/Comparar_spec.rb
+++ spec/Comparar_spec.rb
@@ -1,3 +1,7 @@
-RSpec.describe Comparar do
-    
+RSpec.describe Comparable do
+    before :all do
+    		@alimento1 = Comida.new("Kellogs",100.0,30.0,7.0,4.0,2.0,1.0,71.0,36.0,0.0,46.0,4.0,0.0,0.9,1.0)
+    		@alimento2 = Comida.new("Chocapic",100.0,35.0,7.5,4.5,2.0,1.0,80.0,40.0,0.0,36.0,4.3,1.0,0.7,2.0)
+  	end  
+  	
 end
