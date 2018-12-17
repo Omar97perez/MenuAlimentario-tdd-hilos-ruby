38dd9c9da9cf18c14e12237404e5b7a590d20f09
alu0100945645 <alu0100945645@ull.edu.es>
2018-12-17 23:20:17 +0000

    Craeción de lo necesario para nuevo fichero

--- lib/alimentos.rb
+++ lib/alimentos.rb
@@ -2,6 +2,7 @@ require "alimentos/version"
 require_relative "../lib/alimentos/alimentos.rb" 
 require_relative "../lib/alimentos/antropometricos.rb"
 require_relative "../lib/alimentos/lista.rb"
+require_relative "../lib/alimentos/array.rb"
 
 module Alimentos
 
